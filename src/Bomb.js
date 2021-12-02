9dcad95542bcc9413bbbb748b30be8fed48c6167
Claudia <claudiaruizux@gmail.com>
2021-12-02 20:25:03 +1100

    completed intial state

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
-// your Bomb code here!
+
+
+import React from 'react';
+
+class Bomb extends React.Component {
+    constructor(props) {
+        super()
+
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    updateSeconds = () => {
+        this.setState({
+            secondLeft: this.state.secondsLeft -1
+        })
+    }
+
+    render() {
+        const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+
+        return (
+            <div><button onClick={this.setSecondsLeft}>Set Seconds</button> {message}</div>
+        )
+    }
+}
+
+export default Bomb;
